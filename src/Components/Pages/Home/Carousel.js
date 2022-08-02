877be46406dbe099ea1045fcf4016c67787f5640
AhshanHabib26 <habib.net27@gmail.com>
2022-08-02 15:07:07 +0600

    Banner Update

--- src/Components/Pages/Home/Carousel.js
+++ src/Components/Pages/Home/Carousel.js
@@ -1,43 +1,18 @@
 import React from "react";
-import BannerImg from "../../../images/Banner/Banner (1).png";
-import BannerImg1 from "../../../images/Banner/Banner (2).png";
-import BannerImg2 from "../../../images/Banner/Banner (3).png";
+import Styles from "../../Styles/Carousel.module.css";
 
 const Carousel = () => {
   return (
-    <div className=" bg-neutral">
-      <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-screen ">
-          <img src={BannerImg} className="w-screen" alt="" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-screen">
-          <img src={BannerImg1} className="w-screen" alt="" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-screen">
-          <img src={BannerImg2} className="w-screen" alt="" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle">
-              ❯
-            </a>
+    <div className={Styles.BannaerImage}>
+      <div class="hero min-h-screen">
+        <div class="hero-content text-center bg-primary rounded-3xl opacity-80 mx-5 lg:mx-0">
+          <div class="max-w-xl py-4">
+            <h1 className=" text-white text-lg lg:text-2xl">Explore The Best Parts Collection</h1>
+            <h1 class="text-5xl lg:text-6xl text-white font-bold">Accessories Parts</h1>
+            <p class="py-6 text-white">
+              Like Saftey Equipmnet, OEM & ODM, Copper Wire, Solid Square
+              Torison Axle Round Light, Tight Tolarence Meatal Auto Holder.
+            </p>
           </div>
         </div>
       </div>
